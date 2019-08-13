741a74fee13a232fc8df0cd0253c87213a4e9169
Rogelio Morrell <molekilla@gmail.com>
2019-08-13 11:39:35 -0500

    adds filter

--- module.js
+++ module.js
@@ -1,6 +1,7 @@
 const { gql } = require('apollo-server');
 const fs = require('fs');
 const connexql = fs.readFileSync('./schemas/connex.gql');
+const GraphQLJSON = require('graphql-type-json');
 
 // The GraphQL schema
 const typeDefs = gql(connexql.toString('utf8'));
@@ -9,6 +10,7 @@ const typeDefs = gql(connexql.toString('utf8'));
 module.exports = (connex) => {
     // A map of functions which return data for the schema.
     const resolvers = {
+        JSON: GraphQLJSON,
         Query: {
             connexVersion: () => connex.version,
             genesis: () => connex.thor.genesis,
@@ -19,6 +21,35 @@ module.exports = (connex) => {
                     address,
                     ...resp,
                 }
+            },
+            filter: async (obj, input) => {                
+                const { kind, range, order, limit, criterias } = input.filter;
+                const filter = connex.thor.filter(kind);
+                if (range) {
+                    filter.range({
+                        unit: range.unit === 'block' ? range.unit : 'time',
+                        from: range.from,
+                        to: range.to,
+                    });
+                }
+
+                if (order) {
+                    filter.order(order);
+                } else {
+                    filter.order('asc');
+                }
+
+                const query = criterias.map(i => {
+                    return {
+                        ...i
+                    };
+                });
+                if (query) {
+                    filter.criteria(query);
+                }
+
+                const logs = await filter.apply(0, limit | 100);
+                return logs;
             }
         },
         Account: {
