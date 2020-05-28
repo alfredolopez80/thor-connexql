309aeb2efd692648c7bde84b364d9d5c8d8f016c
Rogelio <molekilla@gmail.com>
2020-05-28 08:59:36 -0500

    fixing sendRawTransaction args

--- src/transaction.ts
+++ src/transaction.ts
@@ -4,7 +4,7 @@ import { ethers } from 'ethers';
 const Web3 = require('web3');
 const { thorify } = require('thorify');
 
-export const sendRawTransaction = () => async (payload: string) => {
+export const sendRawTransaction = () => async (obj, payload: string) => {
     const thor = thorify(new Web3(), process.env.THOR_URL || `https://thor-staging.decent.bet`);
     const res = await thor.eth.sendSignedTransaction(payload);
 
