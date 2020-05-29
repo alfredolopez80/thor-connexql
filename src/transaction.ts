3cbaf3363bd599cf893125d752b57dd9a01a0289
Rogelio <molekilla@gmail.com>
2020-05-28 20:47:08 -0500

    adds missing param name

--- src/transaction.ts
+++ src/transaction.ts
@@ -4,9 +4,10 @@ import { ethers } from 'ethers';
 const Web3 = require('web3');
 const { thorify } = require('thorify');
 
-export const sendRawTransaction = () => async (obj, payload: string) => {
+export const sendRawTransaction = () => async (obj, payload: any) => {
+    const { data } = payload;
     const thor = thorify(new Web3(), process.env.THOR_URL || `https://thor-staging.decent.bet`);
-    const res = await thor.eth.sendSignedTransaction(payload);
+    const res = await thor.eth.sendSignedTransaction(data);
 
     return res.transactionHash;
 }
