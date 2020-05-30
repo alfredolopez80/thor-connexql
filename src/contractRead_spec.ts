82ecf6f6db0759264d651ad0976477a94d31da88
Rogelio <molekilla@gmail.com>
2020-05-30 10:51:34 -0500

    fixing specs to pass tsc

--- src/contractRead_spec.ts
+++ src/contractRead_spec.ts
@@ -19,7 +19,7 @@ describe("#contractRead specs", function () {
         );
 
         console.log(paramsHex)
-        const resp = await contractRead()(energyContractAddress, balanceOfAbiSignature, paramsHex);
+        const resp = await contractRead()(null, { energyContractAddress, balanceOfAbiSignature, paramsHex });
 
         expect(resp).equal('5000000125000000000000');
     });
